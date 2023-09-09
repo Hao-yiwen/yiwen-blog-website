73cd03b73971d53560b0ff93e987cbc1d284da6e
yw.hao <yw.hao@trip.com>
2023-09-10 01:30:47 +0800

    feat: change blog homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,25 +2,27 @@ import React from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
 
 import styles from './index.module.css';
 
 function Homepage() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={styles.heroBanner}>
-    </header>
-  );
+    const { siteConfig } = useDocusaurusContext();
+    return (
+        <header className={styles.pageContainer}>
+            <div className={styles.pageCenter}>
+                <div className={styles.pageFont}>Lemo, I want to fly.</div>
+                <Link to="/docs/web/intro" className={styles.link}>
+                    点击开始
+                </Link>
+            </div>
+        </header>
+    );
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <Homepage />
-    </Layout>
-  );
+    return (
+        <div className={styles.container}>
+            <Homepage />
+        </div>
+    );
 }
